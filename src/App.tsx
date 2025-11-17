3cff7b4f90567fdb8e35ba593f6a83fdf8ec1d7c
Lippo <lippo997@gmail.com>
2025-11-17 16:37:22 +0100

    chore: use generated types for type environment

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,28 @@ import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
 import { Editor } from "./pages/Editor";
-import MotuPatient from "./MotuPatient.json";
-import {
-  fetchStructureDefinition,
-  parseStructureDefinition,
-} from "src-common/structure-definition-utils";
 import type { Resource } from "src-common/fhir-types";
 import { TypeEnvironmentContext } from "./providers/TypeEnvironmentProvider";
 import {
   SimpleTypeEnvironment,
   type TypeEnvironment,
 } from "./model/type-environment";
-import type { TypeMap } from "./model/type-map";
-import type { URL } from "src-common/strict-types";
 
-const names = [
-  "Identifier",
-  "Patient",
-  "Bundle",
-  "Resource",
-  "Observation",
-  "Encounter",
-] as string[];
-
-const staticSds = names.map(
-  (x) => `https://hapi.fhir.org/baseR4/StructureDefinition/${x}?_format=json`,
-);
+const modules = import.meta.glob("../src-generated/metadata/*.json", {
+  eager: true,
+});
 
 function App() {
   const [typeEnv, setTypeEnv] = useState<TypeEnvironment | null>(null);
 
   useEffect(() => {
-    Promise.all(staticSds.map(fetchStructureDefinition))
-      .then((entries) => entries.filter((t): t is Resource => Boolean(t)))
-      .then((entries) =>
-        Object.fromEntries(entries.map((t) => [t.url, t] as [URL, Resource])),
-      )
-      .then((entries) => ({
-        ...entries,
-        [MotuPatient.url]: parseStructureDefinition(MotuPatient) as Resource,
-      }))
-      .then((typeMap) => new SimpleTypeEnvironment(typeMap))
-      .then((typeEnv) => setTypeEnv(typeEnv));
-  }, [staticSds]);
+    const typeMap = Object.fromEntries(
+      Object.entries(modules).map(([_, obj]) => {
+        return [(obj as Resource).url, obj] as [string, Resource];
+      }),
+    );
+    setTypeEnv(new SimpleTypeEnvironment(typeMap));
+  }, []);
 
   return (
     <MantineProvider>
