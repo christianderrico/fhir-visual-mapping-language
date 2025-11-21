546cb1e99bdce38a228df09e6741d511432400f1
christianderrico <christian.derrico@studio.unibo.it>
2025-11-21 09:32:31 +0100

    test: test TypeTree

--- test/model/simple-type-tree.test.ts
+++ test/model/simple-type-tree.test.ts
@@ -1,92 +1,157 @@
-import { SimpleTypeNode, SimpleTypeTree } from "../../src/model/type-tree";
+import {
+  SimpleTypeNode,
+  SimpleTypeTree,
+  TypeNode,
+} from "../../src/model/type-tree";
 import type { Resource } from "../../src-common/fhir-types";
 import type { TypeMap } from "../../src/model/type-map";
-import { TypeNode } from "../../src/model/type-tree";
+
 import { test, describe, expect, beforeAll } from "vitest";
 import { readdirSync, readFileSync } from "fs";
 import { join } from "path";
 import { URL, url } from "../../src-common/strict-types";
+import { partition } from "../../src/utils/functions";
 
 describe("SimpleTypeTree", () => {
-  const folder = "C:\\Users\\chris\\visual-fml\\src-generated\\metadata";
-  const modules = readdirSync(folder)
-    .filter((f) => f.endsWith(".json"))
-    .map((fName) => {
-      const fullPath = join(folder, fName);
-      const content = readFileSync(fullPath, "utf-8");
-      return JSON.parse(content);
-    });
+  const metadataFolder = ".\\src-generated\\metadata";
 
-  const typeMap: TypeMap = Object.fromEntries(
-    Object.entries(modules).map(([_, obj]) => {
-      return [(obj as Resource).url, obj] as [string, Resource];
-    }),
+  const loadModules = (folder: string) =>
+    readdirSync(folder)
+      .filter((f) => f.endsWith(".json"))
+      .map((file) => {
+        const content = readFileSync(join(folder, file), "utf-8");
+        return JSON.parse(content) as Resource;
+      });
+
+  const toEntry = (r: Resource): [string, Resource] => [r.url, r];
+  const asNode = (value: Resource) => new SimpleTypeNode(value);
+
+  const pickRandom = <T>(arr: T[]): T =>
+    arr[Math.floor(Math.random() * arr.length)];
+
+  const nodeName = (node: TypeNode) => node.value.name;
+
+  const modules = loadModules(metadataFolder);
+  const entries = modules.map(toEntry);
+
+  const [resourceEntries, elementEntries] = partition(
+    entries,
+    ([, res]) => res.kind === "resource",
   );
 
-  const node2Name: (node: TypeNode) => string = (node) => node.value.name;
+  const resourceMap: TypeMap = Object.fromEntries(resourceEntries);
+  const elementMap: TypeMap = Object.fromEntries(elementEntries);
 
-  let tree: SimpleTypeTree;
-  let nodes: Record<string, any>;
-  let allNodes: TypeNode[];
-  let patientURL: URL = url("http://hl7.org/fhir/StructureDefinition/Patient");
-  let domainResourceURL: URL = url(
-    "http://hl7.org/fhir/StructureDefinition/DomainResource",
+  let resourceTree: SimpleTypeTree;
+  let elementTree: SimpleTypeTree;
+
+  let resourceNodes: TypeNode[];
+  let elementNodes: TypeNode[];
+
+  const patientURL: URL = url(
+    "http://hl7.org/fhir/StructureDefinition/Patient",
+  );
+  const booleanURL: URL = url(
+    "http://hl7.org/fhir/StructureDefinition/boolean",
   );
 
   beforeAll(() => {
-    tree = new SimpleTypeTree(typeMap);
-    allNodes = Object.values(typeMap).map(
-      (resource) => new SimpleTypeNode(resource),
-    );
-    nodes = Object.fromEntries(allNodes.map((n) => [n.value.url, n]));
+    resourceTree = new SimpleTypeTree(resourceMap);
+    elementTree = new SimpleTypeTree(elementMap);
+
+    resourceNodes = Object.values(resourceMap).map(asNode);
+    elementNodes = Object.values(elementMap).map(asNode);
   });
 
-  test("should identify the root node", () => {
-    expect(tree.root.value.name).toBe("Resource");
+  describe("root handling", () => {
+    test("identifies correct root", () => {
+      expect(resourceTree.root.value.name).toBe("Resource");
+      expect(elementTree.root.value.name).toBe("Element");
+    });
+
+    test("root has no father", () => {
+      expect(resourceTree.root.father).toBeUndefined();
+      expect(elementTree.root.father).toBeUndefined();
+    });
   });
 
-  test("should return all nodes", () => {
-    expect(tree.getAllNodes().map(node2Name).sort()).toEqual(
-      allNodes.map(node2Name).sort(),
-    );
+  describe("node listing", () => {
+    test("returns all nodes", () => {
+      const rNames = resourceNodes.map(nodeName).sort();
+      const rTreeNames = resourceTree.getAllNodes().map(nodeName).sort();
+      expect(rTreeNames).toEqual(rNames);
+
+      const eNames = elementNodes.map(nodeName).sort();
+      const eTreeNames = elementTree.getAllNodes().map(nodeName).sort();
+      expect(eTreeNames).toEqual(eNames);
+    });
   });
 
-  test("should have root without father", () => {
-    expect(tree.root.father).toBeUndefined();
+  describe("node retrieval", () => {
+    test("can get node by URL", () => {
+      const target = pickRandom(resourceNodes);
+      expect(resourceTree.getNode(target.value.url)?.value).toEqual(
+        target.value,
+      );
+
+      const targetElement = pickRandom(elementNodes);
+      expect(elementTree.getNode(targetElement.value.url)?.value).toEqual(
+        targetElement.value,
+      );
+    });
+
+    test("can get node by name", () => {
+      const target = pickRandom(resourceNodes);
+      expect(resourceTree.getNode(target.value.name)?.value).toEqual(
+        target.value,
+      );
+
+      const targetElement = pickRandom(elementNodes);
+      expect(elementTree.getNode(targetElement.value.name)?.value).toEqual(
+        targetElement.value,
+      );
+    });
   });
 
-  test("should have leaves without children", () => {
-    expect(tree.getNodeByURL(patientURL)?.children.length).toBe(0);
-  });
+  describe("tree relationships", () => {
+    test("leaves have no children", () => {
+      expect(resourceTree.getNode(patientURL)?.children).toHaveLength(0);
+      expect(elementTree.getNode(booleanURL)?.children).toHaveLength(0);
+    });
 
-  test("should return node by URL", () => {
-    const random_id = Math.floor(Math.random() * allNodes.length);
-    const node = allNodes[random_id];
-    expect(tree.getNodeByURL(node.value.url)?.value).toEqual(node.value);
-  });
+    test("computes ancestors", () => {
+      expect(resourceTree.getAncestors("Patient").map(nodeName)).toEqual([
+        "Resource",
+        "DomainResource",
+      ]);
 
-  test("should return ancestors of a given node", () => {
-    const ancestors = ["Resource", "DomainResource"];
-    const node = tree.getNodeByURL(patientURL)!;
-    expect(tree.getAncestors(node).map(node2Name)).toEqual(ancestors);
-  });
+      expect(elementTree.getAncestors("Dosage").map(nodeName)).toEqual([
+        "Element",
+        "BackboneElement",
+      ]);
+    });
 
-  test("should return descendants of a given node", () => {
-    const toExclude = [
-      "Parameters",
-      "Bundle",
-      "Binary",
-      "Resource",
-      "DomainResource",
-    ];
-    const resources = modules
-      .map((res) => res.name)
-      .filter((res) => !toExclude.includes(res));
-    const node = tree.getNodeByURL(domainResourceURL);
-    const descendants = tree
-      .getDescendants(tree.getNodeByURL(domainResourceURL)!)
-      .map(node2Name);
+    test("computes descendants", () => {
+      const excluded = [
+        "Parameters",
+        "Bundle",
+        "Binary",
+        "Resource",
+        "DomainResource",
+      ];
 
-    expect(resources.sort()).toEqual(descendants.sort());
+      const allResources = resourceEntries
+        .map(([, res]) => res.name)
+        .filter((name) => !excluded.includes(name));
+
+      const descendants = resourceTree
+        .getDescendants("DomainResource")
+        .map(nodeName);
+
+      expect(descendants.sort()).toEqual(allResources.sort());
+
+      const uriDescendants = elementTree.getDescendants("uri").map(nodeName);
+      expect(uriDescendants).toEqual(["canonical", "oid", "url", "uuid"]);
+    });
   });
 });
